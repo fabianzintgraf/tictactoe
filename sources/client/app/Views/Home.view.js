dc63f92280522fe28568e3dc80632a9dd0dcc07e
Fabian Zintgraf <fabian.zintgraf@check24.de>
2015-10-20 17:40:58 +0200

    Add player displaying functionality + Add functionality to create new game.

--- sources/client/app/Views/Home.view.js
+++ sources/client/app/Views/Home.view.js
@@ -1,26 +1,57 @@
 import React from 'react';
+import { startGameAction } from '../Actions/StartGame.action';
+import gameStore from '../Stores/Game.store';
 
 class Home extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = { players: [] };
+  }
+
+  componentDidMount() {
+    gameStore.addChangeListener(this.updateState.bind(this));
+  }
+
+  componentWillUnmount() {
+    gameStore.removeChangeListener(this.updateState.bind(this));
+  }
+
+  updateState() {
+    this.setState({
+      players: gameStore.getPlayers()
+    });
+  }
+
+  onCreateGameExecute(players) {
+    startGameAction.execute(players[0], players[1]);
+  }
+
   render() {
+
 		return (
 			<div>
 				<h1>Welcome to the TicTacToe application</h1>
         <section>
           <h2>First of all we need the names of the two player:</h2>
-          <p>
-            <label for="player1">Name of player1: </label>
-            <input type="text" id="player1" />
-          </p>
-          <p>
-            <label for="player2">Name of player2: </label>
-            <input type="text" id="player2" />
-          </p>
-          <button>Let's play</button>
+          { this.renderPlayer(this.state.players[0], 0) }
+          { this.renderPlayer(this.state.players[1], 1) }
+          <button onClick={this.onCreateGameExecute(this.state.players)}>Lets play...</button>
         </section>
 			</div>
 		);
 	}
+
+  renderPlayer(player, index) {
+    const indexName = 'player' + index;
+    return (
+      <p key={indexName}>
+        <label for={indexName}>Name of player1: </label>
+        <input type="text" id={indexName} value={player} />
+      </p>
+      );
+  }
 }
 
 export default Home;
